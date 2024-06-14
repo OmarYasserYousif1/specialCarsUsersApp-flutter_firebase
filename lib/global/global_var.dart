a8e7d3c90029b9b052bf626d04f12481e06b24aa
OmarYasser999 <omaryasseryousif@gmail.com>
2024-06-14 18:36:13 +0300

    update

--- lib/global/global_var.dart
+++ lib/global/global_var.dart
@@ -5,12 +5,10 @@ String userName = "";
 String userPhone = "";
 String userID = FirebaseAuth.instance.currentUser!.uid;
 
-String googleMapKey = "AIzaSyCxevwjA9FGdoBGXE4sm3o2wCj9H3Y-Wbw";
-String serverKeyFCM = "key=AAAAWrsD34w:APA91bHYYtRvIOMc5VuKBBWJmwgIGwUFrjeclWOwyBfBMaYbQ_ZFRmyZ9bDhmMy4CP1yq4VkbpczU3yIPkCGSpgc3KNuayfIwVUhVoCLM87z5YpkBZTh6YP0PfchVcVQqUmWqgJO1kVD";
+String googleMapKey = "Your api key here";
+String serverKeyFCM = "Your fiirebase cloud messagng key";
 
 const CameraPosition googlePlexInitialPosition = CameraPosition(
   target: LatLng(37.42796133580664, -122.085749655962),
   zoom: 14.4746,
 );
-
-
